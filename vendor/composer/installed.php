63ef6416f2758d350ccc1cf7a568e7eb9f91a6c6
Nicolas Juen <njuen@beapi.fr>
2021-03-29 12:00:10 +0000

    Create tag 1.0.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-version/1.0.4',
+    'version' => 'dev-version/1.0.4',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f38506f8c418239069b19eee1db4b10787e211f5',
+    'name' => 'beapi/algolia-content-exclude',
+  ),
+  'versions' => 
+  array (
+    'beapi/algolia-content-exclude' => 
+    array (
+      'pretty_version' => 'dev-version/1.0.4',
+      'version' => 'dev-version/1.0.4',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f38506f8c418239069b19eee1db4b10787e211f5',
+    ),
+  ),
+);
